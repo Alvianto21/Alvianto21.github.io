f0fa9a71ed2f27f352a43c50388ef4a17ad42689
Alvianto21 <deliverindihom@gmail.com>
2025-04-15 10:43:50 +0700

    inisial commit

---
+++ tes 4.js
@@ -0,0 +1,13 @@
+var angka = 5665431897;
+var tes = "tesalon";
+var angka2 = 11;
+angka2--;
+document.getElementById("gula").innerHTML = angka2;
+console.log(angka);
+alert('selamat datang');
+var lagi = true;
+while(lagi){
+    var nama = prompt('masukkan nama anda:');
+    alert('halo ' + nama);
+    lagi = confirm('coba lagi?');
+}
